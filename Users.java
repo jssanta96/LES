a1449a95c5a8e28a80f25ba4941f306c021735c7
Cuervo <carlos.cuervo@correounivalle.edu.co>
2017-11-08 17:44:38 -0500

    Agregacion de metodos Get y Set a la clase Users

--- Users.java
+++ Users.java
@@ -1,9 +1,73 @@
 
-public class Usuarios{
+public class Usuarios {
 
-    private static String name;
-    private static Sitrng password;
-    private static String type;
+    private String user_name;
+    private String nickName;
+    private Sitrng password;
+    private String type;
+    private String identification;
+    private String state;
+    private String email;
 
-    Usuarios(){};
+    Usuarios() {
+    }
+
+    ;
+
+    public static String getUserName() {
+        return user_name;
+    }
+
+    public static String getNickName() {
+        return nickName;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public static String getType() {
+        return type;
+    }
+
+    public static String getIdentification() {
+        return identification;
+    }
+
+    public static String getState() {
+        return state;
+    }
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public static void setIdentification(String identification) {
+        this.identification = identification;
+    }
+
+    public static void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public static void setUserName(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public static void setType(String type) {
+        this.type = type;
+    }
+
+    public static void setState(String state) {
+        this.state = state;
+    }
+
+    public static void setEmail(String email){
+        this.email = email;
+    }
+
 }
