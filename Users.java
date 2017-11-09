427bff6da18565f4bb89158dee5c52e81db50872
Cuervo <carlos.cuervo@correounivalle.edu.co>
2017-11-09 14:26:07 -0500

    Modificacion de la clase Usuarios

--- Users.java
+++ Users.java
@@ -1,73 +1,74 @@
 
 public class Usuarios {
 
-    private String user_name;
-    private String nickName;
-    private Sitrng password;
-    private String type;
-    private String identification;
-    private String state;
-    private String email;
+        private String user_name;
+        private String nickName;
+        private String password;
+        private String type;
+        private String identification;
+        private String state;
+        private String email;
 
-    Usuarios() {
-    }
+        Usuarios() {
+        }
 
-    ;
+        ;
 
-    public static String getUserName() {
-        return user_name;
-    }
+        public  String getUserName() {
+            return user_name;
+        }
 
-    public static String getNickName() {
-        return nickName;
-    }
+        public  String getNickName() {
+            return nickName;
+        }
 
-    public static String getPassword() {
-        return password;
-    }
+        public  String getPassword() {
+            return password;
+        }
 
-    public static String getType() {
-        return type;
-    }
+        public  String getType() {
+            return type;
+        }
 
-    public static String getIdentification() {
-        return identification;
-    }
+        public  String getIdentification() {
+            return identification;
+        }
 
-    public static String getState() {
-        return state;
-    }
+        public  String getState() {
+            return state;
+        }
 
-    public static String getEmail() {
-        return email;
-    }
+        public  String getEmail() {
+            return email;
+        }
 
-    public static void setIdentification(String identification) {
-        this.identification = identification;
-    }
+        public  void setIdentification(String identification) {
+            this.identification = identification;
+        }
 
-    public static void setPassword(String password) {
-        this.password = password;
-    }
+        public  void setPassword(String password) {
+            this.password = password;
+        }
 
-    public static void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
+        public  void setNickName(String nickName) {
+            this.nickName = nickName;
+        }
 
-    public static void setUserName(String user_name) {
-        this.user_name = user_name;
-    }
+        public  void setUserName(String user_name) {
+            this.user_name = user_name;
+        }
 
-    public static void setType(String type) {
-        this.type = type;
-    }
+        public  void setType(String type) {
+            this.type = type;
+        }
 
-    public static void setState(String state) {
-        this.state = state;
-    }
+        public  void setState(String state) {
+            this.state = state;
+        }
 
-    public static void setEmail(String email){
-        this.email = email;
-    }
+        public  void setEmail(String email){
+            this.email = email;
+        }
 
+    
 }
