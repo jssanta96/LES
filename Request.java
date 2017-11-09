212322b132ec6e836d57b423fdf20e1f9f3f2889
Cuervo <carlos.cuervo@correounivalle.edu.co>
2017-11-09 14:58:25 -0500

    Creacion de la clase Solicitudes

---
+++ Request.java
@@ -0,0 +1,40 @@
+public class Request {
+    private String user_id;
+    private String serial;
+    private String start_date;
+    private String end_date;
+
+    Request(){};
+
+    public String getUser_id(){
+        return user_id;
+    }
+
+    public String getSerial(){
+        return serial;
+    }
+
+    public String getStart_date(){
+        return start_date;
+    }
+
+    public String getEnd_date(){
+        return end_date;
+    }
+
+    public void setUser_id(String user_id){
+        this.user_id=user_id;
+    }
+
+    public void setSerial(String serial){
+        this.serial=serial;
+    }
+
+    public void setStart_date(String start_date){
+        this.start_date=start_date;
+    }
+
+    public void setEnd_date(String end_date){
+        this.end_date=end_date;
+    }
+}
