512af382865f134c72843337c4ee8ed8d43dd0de
carlosacg <carlos.cuervo@correounivall.edu.co>
2017-11-10 14:44:39 -0500

    Creacion del Dao de equipos

---
+++ Dao/EquipmentDao.java
@@ -0,0 +1,114 @@
+package Dao;
+
+import Logica.Equipment;
+import Logica.Project;
+
+import java.sql.*;
+
+public class EquipmentDao {
+    FachadaBD fachada;
+
+
+    public EquipmentDao(){
+        fachada = new FachadaBD();
+    }
+
+
+
+    public int saveEquipment(Equipment equipment){
+        String save_sql;
+        int numberRows=0;
+
+        save_sql="INSERT INTO equipment VALUES('"+
+                equipment.getSerial()+"','"+equipment.getName()+"','"
+                +equipment.getDescription()+"','"+equipment.getState()+"');";
+
+        try{
+            Connection conn= fachada.getConnetion();
+            Statement sentence = conn.createStatement();
+
+            numberRows= sentence.executeUpdate(save_sql);
+            System.out.println("up" + numberRows);
+            return numberRows;
+
+        }catch(SQLException e){
+            System.out.println(e);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return -1;
+    }
+
+
+
+    public Equipment viewEquipment(String serial){
+        Equipment equipment= new Equipment();
+        String sql_select;
+        sql_select="SELECT serial,name,description,state"+
+                " FROM equipment WHERE serial='"+serial+"';";
+
+        try{
+            Connection conn= fachada.getConnetion();
+            System.out.println("consultando en la bd");
+            Statement sentence = conn.createStatement();
+            ResultSet tabla = sentence.executeQuery(sql_select);
+
+            while(tabla.next()){
+                equipment.setSerial(tabla.getString(1));
+
+                equipment.setName(tabla.getString(2));
+
+                equipment.setDescription(tabla.getString(3));
+
+                equipment.setState(tabla.getString(4));
+
+                System.out.println("ok");
+            }
+
+            return equipment;
+        }
+        catch(SQLException e){
+            System.out.println(e);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return null;
+    }
+
+
+
+    public void updateEquipment(String serial, String name, String description, String state){
+
+        String sql_select;
+        sql_select="UPDATE equipment SET serial ='" + serial +  "', name = '"+ name +"', description = '"+ description +
+                "', state ='" + state +" WHERE  serial='"+ serial +"';";
+        try{
+            Connection conn= fachada.getConnetion();
+            System.out.println("Update in the bd");
+            Statement sentencia = conn.createStatement();
+            sentencia.executeUpdate(sql_select);
+
+        }
+        catch(SQLException e){ System.out.println(e); }
+        catch(Exception e){ System.out.println(e); }
+
+    }
+
+
+    public void deleteEquipment(String serial){
+
+        String sql_select;
+        sql_select="DELETE FROM equipment WHERE  serial='"+ serial+"';";
+        try{
+            Connection conn= fachada.getConnetion();
+            System.out.println("Delete in the bd");
+            Statement sentencia = conn.createStatement();
+            sentencia.executeUpdate(sql_select);
+        }
+        catch(SQLException e){ System.out.println(e); }
+        catch(Exception e){ System.out.println(e); }
+
+    }
+}
