b46044a33e682a5d9932feede4b08ded18245824
Carlos Andres Cuervo <carlos.cuervo@correounivalle.edu.co>
2017-11-28 18:56:25 -0500

    Actualizacion de la clase multa

--- Logica/Mult.java
+++ Logica/Mult.java
@@ -2,15 +2,11 @@ package Logica;
 
 public class Mult {
 
-    private int id_user;
     private int id_request;
     private double value;
 
     public Mult(){};
 
-    public int getId_user(){
-        return id_user;
-    }
 
     public int getId_request(){
         return id_request;
@@ -18,10 +14,6 @@ public class Mult {
 
     public double getValue(){
         return value;
-    }
-
-    public void setId_user(int id_user){
-        this.id_user=id_user;
     }
 
     public void setId_request(int id_request){
